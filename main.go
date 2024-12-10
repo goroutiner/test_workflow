dce0cf8fdc92854c369828b565b79be3db8a92b9
Alexey <bakovin26@list.ru>
2024-12-11 00:10:49 +0300

    test on new branch

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-    "sync"
-)
-
 func main() {
-    var wg sync.WaitGroup
-    wg.Add(10)
 
-    for i := 0; i < 10; i++ {
-        go func() {
-            i++
-            wg.Done()
-        }()
-    }
-
-    wg.Wait()
 }
 
 func MaxInt(a, b int) int {
